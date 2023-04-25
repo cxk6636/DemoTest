2fbc7e781ea2ab528875465c47f9e14ad3080f7c
chenxiaokang <chenxiaokang@beyondcent.com>
2023-04-25 18:25:10 +0800

    kafka

--- src/main/java/com/kafka/KafkaC.java
+++ src/main/java/com/kafka/KafkaC.java
@@ -1,39 +1,35 @@
 package com.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-
-import java.util.Collections;
-import java.util.Properties;
-
-/**
- * @author cxk
- * 2023/1/10 14:40
- */
 import org.apache.kafka.clients.consumer.*;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class KafkaC {
     public static void main(String[] args) {
-        String topicName = "my-topic";
-        // Set up the consumer configuration properties
+        String topicName = "test-topic";
+        String groupName = "test-group";
+
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "my-group");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("group.id", groupName);
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
 
-        // Create the Kafka consumer instance
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList(topicName));
 
-        // Subscribe to the topic
-        consumer.subscribe(Collections.singletonList(topicName));
-
-        // Continuously poll for new messages
         while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(1000);
+            ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records) {
-                System.out.printf("Received message with value %s from partition %d and offset %d\n", record.value(), record.partition(), record.offset());
+                System.out.println("topic = " + record.topic() + ", partition = " + record.partition()
+                        + ", offset = " + record.offset() + ", key = " + record.key() + ", value = " + record.value());
             }
+            // 手动提交偏移量
+            consumer.commitSync();
         }
     }
 }
--- src/main/java/com/kafka/KafkaP.java
+++ src/main/java/com/kafka/KafkaP.java
@@ -1,56 +1,29 @@
 package com.kafka;
 
-import java.util.Properties;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.KafkaAdminClient;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-
-/**
- * @author cxk
- * 2023/1/10 14:38
- */
-
-import java.util.Properties;
 import org.apache.kafka.clients.producer.*;
+import java.util.Properties;
 
 public class KafkaP {
     public static void main(String[] args) {
-        String topicName = "my-topic";
-        String key = "key1";
-        String value = "Hello, Kafka!";
+        String topicName = "test-topic";
+        String key = "test-key";
+        String value = "test-value";
 
-        // Set up the producer configuration properties
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
 
-        // Create the Kafka producer instance
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-        // Create the message to send
+        Producer<String, String> producer = new KafkaProducer<>(props);
         ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
-
-        // Send the message asynchronously
-        producer.send(record, new Callback() {
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                if (exception == null) {
-                    System.out.printf("Sent message with offset %d to partition %d\n", metadata.offset(), metadata.partition());
-                } else {
-                    System.out.println("Error sending message: " + exception.getMessage());
-                }
-            }
-        });
-
-        // Close the producer
+        producer.send(record);
         producer.close();
     }
 }
